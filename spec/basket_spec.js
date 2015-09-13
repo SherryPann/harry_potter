11f9ee8a418661285a45bc00bb6cbfdf1d3d3639
sherrypan <sherrypann@outlook.com>
2015-09-13 16:59:54 +0800

    correct the test require basket_book.js to the test

--- spec/basket_spec.js
+++ spec/basket_spec.js
@@ -1,9 +1,9 @@
 var Basket = require("../src/basket.js");
+var Basket_Book = require("../src/basket_book.js");
 
 describe("Basket",function(){
     var basket;
-    var books = [{name:'harryOne',count:2},{name:"harryTwo",count:3}];
-
+    var books = [new Basket_Book('harryOne',2),new Basket_Book("harryTwo",3)];
     beforeEach(function(){
         basket = new Basket(books);
     });
@@ -16,10 +16,17 @@ describe("Basket",function(){
 
     describe("addBook",function(){
        it("can add book to books",function(){
-           var books = [{name:'harryOne',count:3},{name:"harryTwo",count:3}];
-
-           basket.addBook('harryOne');
-           expect(basket.books).toEqual(books);
+           basket.addBook(new   Basket_Book('harryOne',1));
+           expect(basket.books).toEqual([new Basket_Book('harryOne',3),new Basket_Book("harryTwo",3)]);
        });
+    });
+
+    describe("getSumtotal",function(){
+        it("should return sumtotal after best discount",function(){
+            var books = [new  Basket_Book('harryOne',2), new  Basket_Book("harryTwo",3)];
+            basket = new Basket(books);
+            var sumTotal = basket.getSumTotal();
+            expect(sumTotal).toEqual("38.40");
+        });
     });
 })
