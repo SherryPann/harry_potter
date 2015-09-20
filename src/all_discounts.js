47d5172e3563f84c29242d4ff15ef2ab137a9e43
sherrypan <sherrypann@outlook.com>
2015-09-20 11:32:56 +0800

    add blank between  parameters in src/all_discount.js

--- src/all_discounts.js
+++ src/all_discounts.js
@@ -3,12 +3,11 @@ var Discount = require("./discount.js");
 function loadAllDiscounts(){
 
     return [
-        new Discount(2,0.05),
-        new Discount(3,0.1),
-        new Discount(4,0.2),
-        new Discount(5,0.25)
+        new Discount( 2, 0.05),
+        new Discount( 3, 0.1),
+        new Discount( 4, 0.2),
+        new Discount( 5, 0.25)
     ]
-
 }
 
 module.exports = loadAllDiscounts();
